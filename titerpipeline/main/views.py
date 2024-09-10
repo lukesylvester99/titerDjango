a8ff3e4299fa86107cad4a04c8de1a604acab314
Luke Sylvester <145699572+lukesylvester99@users.noreply.github.com>
2024-09-10 16:35:51 -0700

    started trying to incorportate html/css templates

--- titerpipeline/main/views.py
+++ titerpipeline/main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(response):
-    return HttpResponse("Hello World")
+def home(request):
+    return render(request, "home.html")
 
 def v1(response):
     return HttpResponse("This is V1")
