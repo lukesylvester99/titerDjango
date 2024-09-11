9bb51c7ea4987b2b3045ba7de485c9e2358c8c68
Luke Sylvester <145699572+lukesylvester99@users.noreply.github.com>
2024-09-11 16:17:31 -0700

    Made superuser for admin db. See settings 4 login

--- titerpipeline/titerpipeline/settings.py
+++ titerpipeline/titerpipeline/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+####
+#Login for Admin Page = {User Name:russell, Password:wolbachia}. all lowercase.
+####
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
