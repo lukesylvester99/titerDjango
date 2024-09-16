0633759d952532172c71a84ab504cb67ec814b40
Luke Sylvester <145699572+lukesylvester99@users.noreply.github.com>
2024-09-16 14:29:55 -0700

    created 1st models and registered then w admin pg

--- titerpipeline/main/admin.py
+++ titerpipeline/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Experiment, Sample,Sample_Metadata,Read_Pair
+
+#admin.site.register(Experiment)
+admin.site.register(Sample)
+admin.site.register(Sample_Metadata)
+admin.site.register(Experiment)
+admin.site.register(Read_Pair)
--- titerpipeline/main/models.py
+++ titerpipeline/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Experiment(models.Model):
+    name = models.CharField(max_length=255)
+
+class Sample(models.Model):
+    sample_id = models.CharField(max_length=30)
+    created_date = models.DateField()
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+
+class Sample_Metadata(models.Model):
+    sample_id = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    metadata = models.JSONField()
+
+class Read_Pair(models.Model):
+    read1_path = models.CharField(max_length=255)
+    read2_path = models.CharField(max_length=255)
+    sample_id = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    plate_number = models.IntegerField()
